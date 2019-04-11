34792764a78644f8a13c89735fa307c19b8477b4
Nathan Robinson <nrobinson2000@me.com>
2019-04-11 10:37:05 -0400

    at 10:37 of 2019-04-11

--- src/robot-arm.ino
+++ src/robot-arm.ino
@@ -1,20 +1,44 @@
 /*
- * Project robot-arm
- * Description:
- * Author:
- * Date:
+ * Project: robot-arm
+ * Description: Our project for the Rocket.Build Community Hackathon
+ * Authors: Nathan Robinson, J Tarness
+ * Date: 2019-04-13
  */
 
+// Project Settings
+#define USE_WIFI 1 // Comment out to disable WIFI
+
+// Library Includes
 #include "Adafruit-MotorShield-V2.h"
 
+// Particle System Configuration
+SYSTEM_MODE(SEMI_AUTOMATIC);
+SYSTEM_THREAD(ENABLED);
+
+// Library Instances
+Adafruit_MotorShield motors;
+
+// Startup Processes
+void startupTasks()
+{
+  Serial.begin(115200);
+}
+STARTUP(startupTasks());
+
 // setup() runs once, when the device is first turned on.
-void setup() {
+void setup()
+{
   // Put initialization like pinMode and begin functions here.
 
+  motors.begin();
+
+#ifdef USE_WIFI
+  Particle.connect();
+#endif
 }
 
 // loop() runs over and over again, as quickly as it can execute.
-void loop() {
+void loop()
+{
   // The core of your code will likely live here.
-
 }
